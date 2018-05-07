f288c606a91acba86c77b242c2754bac8b40c016
zhangliang <914342085@qq.com>
2018-05-07 08:04:41 +0800

    将article表content字段改成text类型

--- models/article.js
+++ models/article.js
@@ -3,7 +3,7 @@
 module.exports = (sequelize, DataTypes) => {
   var Article = sequelize.define('Article', {
     title: DataTypes.STRING,
-    content: DataTypes.STRING
+    content: DataTypes.TEXT
   }, {});
   Article.associate = function(models) {
     // associations can be defined here
